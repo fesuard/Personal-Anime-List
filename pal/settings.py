c3a97dff4e1b59f8a5a23e0d3de270a994f21c7f
Fes <example.email@example>
2024-04-23 22:17:19 +0300

    Procfile added

--- pal/settings.py
+++ pal/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l5_s@^ewah04b_^ur!5u(9_01l$*3v71o8ucf=4al^g-jw-3$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://personal-anime-list-03666f13882e.herokuapp.com', 'localhost']
 
 # Application definition
 
